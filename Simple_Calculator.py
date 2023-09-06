44fe895b19b0dfb3fa2cb6105988189cfdedc760
SAI JAGADESH S <69421242+sai-jagadesh@users.noreply.github.com>
2023-09-06 08:06:41 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -11,25 +11,14 @@ def calculate():
         entry.delete(0, tk.END)  # Clear the input field
         entry.insert(tk.END, "Error")
 
-# Function to add a character to the input field
-def add_character(character):
-    entry.insert(tk.END, character)
-
-# Function to handle keyboard events
-def on_key_press(event):
-    key = event.char
-    if key == '\r':  # Enter key
-        calculate()
-    elif key in '0123456789.*/+-':
-        add_character(key)
-
 # Create the main application window
 app = tk.Tk()
 app.title("Calculator")
 
-# Create an input field
+# Create an input field and put cursor in it
 entry = tk.Entry(app, width=25, font=("Helvetica", 16))
 entry.grid(row=0, column=0, columnspan=4)
+entry.focus_set()  # Set focus to the entry field
 
 # Create buttons for numbers and operators
 buttons = [
@@ -44,7 +33,7 @@ col_val = 0
 
 for button in buttons:
     tk.Button(app, text=button, width=6, height=2, font=("Helvetica", 16),
-              command=lambda btn=button: add_character(btn) if btn != "=" else calculate()).grid(row=row_val, column=col_val)
+              command=lambda btn=button: entry.insert(tk.END, btn) if btn != "=" else calculate()).grid(row=row_val, column=col_val)
     col_val += 1
     if col_val > 3:
         col_val = 0
@@ -52,9 +41,6 @@ for button in buttons:
 
 # Bind the Enter key to the calculate function
 app.bind('<Return>', lambda event=None: calculate())
-
-# Bind other keys to the on_key_press function
-app.bind('<Key>', on_key_press)
 
 # Start the GUI application
 app.mainloop()
